5236f4f2cf73620d22fe4c772d20bcfd35452486
Kolin63 <theverylastgoodname@gmail.com>
2026-06-15 12:49:30 -0400

    Add custom return macros for all json data check macros

--- src/json_macros.h
+++ src/json_macros.h
@@ -1,7 +1,7 @@
 #ifndef ENDIAN_JSON_MACROS_H_
 #define ENDIAN_JSON_MACROS_H_
 
-// checks that a bool is the proper type
+// checks that a bool is the proper type. custom return
 // params:
 // - const jsmn_iterator _iter
 // assumes the following variables exist:
@@ -10,11 +10,11 @@
 // - const char* file_name
 // - const char* mod_name
 // - int error
-#define END_JSON_CHECK_BOOL(_iter)                                                                      \
+#define END_JSON_CHECK_BOOL_RET(_iter, _ret)                                                            \
   if (_iter.val->type != JSMN_PRIMITIVE) {                                                              \
     log_error("In data type %s from mod %s, %s must be bool (type)", file_name, mod_name, _iter.key);   \
     error++;                                                                                            \
-    continue;                                                                                           \
+    _ret;                                                                                               \
   }                                                                                                     \
   {                                                                                                     \
     char val[2];                                                                                        \
@@ -22,7 +22,43 @@
     if (val[0] != 'f' && val[0] != 't') {                                                               \
       log_error("In data type %s from mod %s, %s must be bool (char)", file_name, mod_name, _iter.key); \
       error++;                                                                                          \
-      continue;                                                                                         \
+      _ret;                                                                                             \
+    }                                                                                                   \
+  }
+
+// checks that a bool is the proper type.
+// params:
+// - const jsmn_iterator _iter
+// assumes the following variables exist:
+// - struct json_iterator* iter
+// - const char* json
+// - const char* file_name
+// - const char* mod_name
+// - int error
+#define END_JSON_CHECK_BOOL(_iter) END_JSON_CHECK_BOOL_RET(_iter, continue);
+
+// checks that a number is the proper type. custom return
+// params:
+// - const jsmn_iterator _iter
+// assumes the following variables exist:
+// - struct json_iterator* iter
+// - const char* json
+// - const char* file_name
+// - const char* mod_name
+// - int error
+#define END_JSON_CHECK_NUMBER_RET(_iter, _ret)                                                          \
+  if (_iter.val->type != JSMN_PRIMITIVE) {                                                              \
+    log_error("In data type %s from mod %s, %s must be number (type)", file_name, mod_name, _iter.key); \
+    error++;                                                                                            \
+    _ret;                                                                                               \
+  }                                                                                                     \
+  {                                                                                                     \
+    char val[2];                                                                                        \
+    jsmn_iterator_get_string(val, 2, json, _iter.val);                                                  \
+    if (val[0] != '-' && (val[0] < '0' || val[0] > '9')) {                                              \
+      log_error("In data type %s from mod %s, %s must be bool (char)", file_name, mod_name, _iter.key); \
+      error++;                                                                                          \
+      _ret;                                                                                             \
     }                                                                                                   \
   }
 
@@ -35,20 +71,20 @@
 // - const char* file_name
 // - const char* mod_name
 // - int error
-#define END_JSON_CHECK_NUMBER(_iter)                                                                    \
-  if (_iter.val->type != JSMN_PRIMITIVE) {                                                              \
-    log_error("In data type %s from mod %s, %s must be number (type)", file_name, mod_name, _iter.key); \
-    error++;                                                                                            \
-    continue;                                                                                           \
-  }                                                                                                     \
-  {                                                                                                     \
-    char val[2];                                                                                        \
-    jsmn_iterator_get_string(val, 2, json, _iter.val);                                                  \
-    if (val[0] != '-' && (val[0] < '0' || val[0] > '9')) {                                              \
-      log_error("In data type %s from mod %s, %s must be bool (char)", file_name, mod_name, _iter.key); \
-      error++;                                                                                          \
-      continue;                                                                                         \
-    }                                                                                                   \
+#define END_JSON_CHECK_NUMBER(_iter) END_JSON_CHECK_NUMBER_RET(_iter, continue);
+
+// checks that a string is the proper type. custom return
+// params:
+// - const jsmn_iterator _iter
+// assumes the following variables exist:
+// - const char* file_name
+// - const char* mod_name
+// - int error
+#define END_JSON_CHECK_STRING_RET(_iter, _ret)                                                   \
+  if (_iter.val->type != JSMN_STRING) {                                                          \
+    log_error("In data type %s from mod %s, %s must be string", file_name, mod_name, _iter.key); \
+    error++;                                                                                     \
+    _ret;                                                                                        \
   }
 
 // checks that a string is the proper type
@@ -58,11 +94,23 @@
 // - const char* file_name
 // - const char* mod_name
 // - int error
-#define END_JSON_CHECK_STRING(_iter)                                                             \
-  if (_iter.val->type != JSMN_STRING) {                                                          \
-    log_error("In data type %s from mod %s, %s must be string", file_name, mod_name, _iter.key); \
-    error++;                                                                                     \
-    continue;                                                                                    \
+#define END_JSON_CHECK_STRING(_iter) END_JSON_CHECK_STRING_RET(_iter, continue);
+
+// checks that string length is: min <= length <= max. custom return
+// params:
+// - const jsmn_iterator _iter
+// assumes the following variables exist:
+// - const char* file_name
+// - const char* mod_name
+// - int error
+#define END_JSON_CHECK_STRING_LENGTH_RET(_iter, _ret, min, max)                                   \
+  {                                                                                               \
+    int len = _iter.val->end - _iter.val->start;                                                  \
+    if (len < min || len > max) {                                                                 \
+      log_error("In data type %s from mod %s, %s is wrong size", file_name, mod_name, _iter.key); \
+      error++;                                                                                    \
+      _ret;                                                                                       \
+    }                                                                                             \
   }
 
 // checks that string length is: min <= length <= max
@@ -72,15 +120,7 @@
 // - const char* file_name
 // - const char* mod_name
 // - int error
-#define END_JSON_CHECK_STRING_LENGTH(_iter, min, max)                                             \
-  {                                                                                               \
-    int len = _iter.val->end - _iter.val->start;                                                  \
-    if (len < min || len > max) {                                                                 \
-      log_error("In data type %s from mod %s, %s is wrong size", file_name, mod_name, _iter.key); \
-      error++;                                                                                    \
-      continue;                                                                                   \
-    }                                                                                             \
-  }
+#define END_JSON_CHECK_STRING_LENGTH(_iter, min, max) END_JSON_CHECK_STRING_LENGTH_RET(_iter, continue, min, max);
 
 // checks that an array is the proper type. custom return
 // params:
